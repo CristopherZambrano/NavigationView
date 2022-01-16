5d8cce15f73ebbc43551453dd319167070a903c2
CristopherZambrano <79436828+CristopherZambrano@users.noreply.github.com>
2022-01-15 23:45:52 -0500

    Usuario completado

--- app/src/main/java/com/example/navigationview/MainActivity.java
+++ app/src/main/java/com/example/navigationview/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.navigationview;
 
+import com.example.navigationview.usuarios;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText user, pass;
+    Button login;
+    usuarios users = new usuarios();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = findViewById(R.id.user);
+        pass = findViewById(R.id.password);
+        login = findViewById(R.id.button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user.getText().toString().equals(users.getUser1())||user.getText().toString().equals(users.getUser2())){
+                    if (pass.getText().toString().equals(users.getPassword1())){
+                        Intent intent = new Intent(MainActivity.this,inicio.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(MainActivity.this,"Contraseña incorrecta para "+user.getText(), Toast.LENGTH_LONG).show();
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this,"Usuario incorrecto", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/navigationview/usuarios.java
@@ -0,0 +1,36 @@
+package com.example.navigationview;
+
+public class usuarios {
+
+    private String user1 = "Cristopher";
+    private String password1 ="123456";
+    private String tipo1 ="admin";
+private String user2 ="Stiven";
+private String password2 ="123456";
+private String tipo2 ="user";
+
+
+    public String getUser1() {
+        return user1;
+    }
+
+    public String getPassword1() {
+        return password1;
+    }
+
+    public String getTipo1() {
+        return tipo1;
+    }
+
+    public String getUser2() {
+        return user2;
+    }
+
+    public String getPassword2() {
+        return password2;
+    }
+
+    public String getTipo2() {
+        return tipo2;
+    }
+}
